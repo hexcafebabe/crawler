88ad9ab28bb2705e38bb508e5e18bfd3badddcaa
hex cafebabe <xxxcafebabe@gmail.com>
2020-08-23 20:43:23 -0700

    print to csv instead of stdout

--- crawl.py
+++ crawl.py
@@ -5,6 +5,7 @@ from collections import defaultdict
 import praw
 import tokens # reddit API keys
 import post   # my secret collection of subs to post to :-)
+import csv
 
 def main():
   reddit = praw.Reddit(client_id=tokens.client_id,
@@ -13,21 +14,28 @@ def main():
                       user_agent=tokens.user_agent,
                       username=tokens.username)
 
-  for c in post.categories.keys():
-    print('category: ' + c)
-    for sub in post.categories[c]:
-      print('\tsub: /r/' + sub)
-      d = defaultdict(int)
+  fieldnames = ['sub', 'time', 'posts']
 
-      # 300 seems fairly representative atm
-      for submission in reddit.subreddit(sub).top("month", limit=300):
-          time = int(submission.created_utc)
-          time_str = datetime.utcfromtimestamp(time).strftime('%a %H:00')
-          d[time_str] += 1
+  with open('time_list.csv', mode='w') as csv_file:
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    writer.writeheader()
 
-      best_times = sorted(d, key=d.get, reverse=True)[:5]
-      for k in best_times:
-        print('\t\t' + k + ', with ' + str(d[k]) + ' posts.')
+    for c in post.categories.keys():
+      print('working on category: ' + c)
+      for sub in post.categories[c]:
+        f_sub ='/r/' + sub
+        d = defaultdict(int)
+
+        # 300 seems fairly representative atm
+        for submission in reddit.subreddit(sub).top("month", limit=300):
+            time = int(submission.created_utc)
+            time_str = datetime.utcfromtimestamp(time).strftime('%a %H:00')
+            d[time_str] += 1
+
+        best_times = sorted(d, key=d.get, reverse=True)[:5]
+        for k in best_times:
+          writer.writerow({'sub': f_sub, 'time': k, 'posts': str(d[k])})
+          # print('\t\t' + k + ', with ' + str(d[k]) + ' posts.')
 
 if __name__ == "__main__":
   main()
